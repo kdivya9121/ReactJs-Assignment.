02c4bbc1b24d356c82c236938c5f301dc06af62e
kdivya9121 <143094645+kdivya9121@users.noreply.github.com>
2023-08-24 19:26:44 +0530

    Backend/index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors"
+import userRoutes from "./routes/users.js";
+
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/",userRoutes)
+
+app.get("/",(req,res)=> res.send("hello from Express"));
+app.all("*",(req,res)=>res.send("That Routes doesnt exist"))
+
+app.listen(port, ()=> console.log(`server is listening on port: http://localhost:${port}`));
